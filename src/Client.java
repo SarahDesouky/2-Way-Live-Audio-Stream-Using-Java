6860cb73f575b4feb8eabee8b1741f4728b7a72a
SarahDesouky <sarahdesouky94@gmail.com>
2015-10-05 23:19:54 +0200

    Project Updated

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,29 @@
-
+import java.net.*;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine; 
 public class Client {
 
+	static DatagramSocket clientSocket;
+	static TargetDataLine microphone;
+
+
+	public static void main(String args[]) throws Exception 
+	{ 
+		clientSocket = new DatagramSocket(); 
+		InetAddress IPAddress = InetAddress.getByName("localhost"); 
+		AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+		microphone = (TargetDataLine) AudioSystem.getLine(info);
+		microphone.open(format);
+		microphone.start();
+
+		while(true) {
+			byte[] audioData = new byte[microphone.getBufferSize()];
+			microphone.read(audioData, 0, 4096); 
+			DatagramPacket packetToSend = new DatagramPacket(audioData, audioData.length, IPAddress, 9876); 
+			clientSocket.send(packetToSend);
+		}
+	} 
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,28 @@
+import java.net.*;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine; 
 
 public class Server {
-
+	
+	static SourceDataLine speakers;
+	
+	public static void main(String args[]) throws Exception 
+	{ 
+		DatagramSocket serverSocket = new DatagramSocket(9876); 
+		byte[] receiveData = new byte[1024]; 
+		AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+		DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
+        speakers = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+        speakers.open(format);
+        speakers.start();
+		while(true) 
+		{ 
+			DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length); 
+			serverSocket.receive(receivedPacket); 
+			byte[] AudioReceived = receivedPacket.getData(); 
+			speakers.write(AudioReceived, 0, receivedPacket.getLength());
+		} 
+	} 
 }
